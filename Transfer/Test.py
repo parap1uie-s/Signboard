21689f7495ddacfe4044d760b0c2d3dce3b24faa
parap1uie-s <parap1uie-s@users.noreply.github.com>
2018-05-09 11:41:27 +0800

    update'

--- Transfer/Test.py
+++ Transfer/Test.py
@@ -2,24 +2,26 @@ import pandas as pd
 from PIL import Image
 import numpy as np
 import os
-from Model import *
+from Model import ResNet
+from Utils import TestDataGen
 
 if __name__ == '__main__':
     datapath = "/home/Signboard/datasets"
-    test_pd = pd.read_csv(os.path.join(datapath, "test.txt"), sep=' ', names=['filepath'], dtype={"filepath":"str"})
+    shape = 224
 
     model = ResNet((224,224,3))
     model.load_weights("w.h5", by_name=True)
 
     result = open("result.csv", "w+", encoding='UTF-8')
 
-    for row in test_pd.iterrows():
-        r = row[1]
-        f = r['filepath']
-
-        Img = Image.open( os.path.join(datapath,'test', f) ).resize((224, 224),Image.ANTIALIAS)
-        Img = np.expand_dims(np.array( Img ),axis=0) / 255.0
-
-        res = model.predict(Img)
-        max_ind = np.argmax(res[0]).astype("int32") + 1
-        result.write("{} {}\n".format(f,max_ind))
+    gen = TestDataGen(datapath, shape)
+    while True:
+        try:
+            Img, filepath = next(gen)
+            res = model.predict(Img)
+            res = np.argmax(res, axis=1)
+            for k,f in enumerate(filepath):
+                result.write("{} {}\n".format(f,res[k]+1))
+        except Exception as e:
+            print(e)
+            break
--- Transfer/Utils.py
+++ Transfer/Utils.py
@@ -36,6 +36,35 @@ def DataGen(datapath, shape, batch_size=32, phase='train'):
         y = np.array(y)
         yield x, y
 
+def TestDataGen(datapath, shape, batch_size=32):
+    csv_handle = pd.read_csv(os.path.join(datapath, "test.txt"), sep=' ', names=['filepath'],dtype={"filepath":"str"})
+
+    data_num = len(csv_handle)
+    ind = 0
+    while True:
+        x = []
+        y = []
+        if ind >= data_num:
+            break
+        choiced_data = csv_handle.iloc[ind:min(ind+batch_size,data_num),:]
+        for row in choiced_data.iterrows():
+            r = row[1]
+            Img = Image.open(os.path.join(datapath, 'test', r['filepath'])).resize((shape,shape),Image.ANTIALIAS)
+
+            aug =  random.randint(0,2)
+            # 水平翻转
+            if aug == 1:
+                Img = Img.transpose(Image.FLIP_LEFT_RIGHT)
+            elif aug == 2:
+                Img = Img.transpose(Image.FLIP_TOP_BOTTOM)
+            x.append(np.array(Img))
+            y.append(r['filepath'])
+
+        x = np.array(x) / 255.0
+        y = np.array(y)
+        ind += batch_size
+        yield x, y
+
 def moveImg(datapath):
     from sklearn.model_selection import train_test_split
     all_pd = pd.read_csv( os.path.join(datapath, "train.txt"), sep=' ', names=['filepath', 'classid'], dtype={"filepath":"str", "classid":"str"})
