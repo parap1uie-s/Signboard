3539791785fe37c1ad94a8285b5fad13bd71a91b
sfx <shangfangxin0341@link.tyut.edu.cn>
2018-05-22 03:14:48 +0800

    update

--- FeatureEngineering/result_vote.py
+++ FeatureEngineering/result_vote.py
@@ -44,13 +44,15 @@ def GridMergeResult(Submission_path):
     print(bestAcc)
     print(bestComb)
 
-def MergeResult(Submission_path,vote_result):
+def MergeResult(Submission_path):
+    true_label = pd.read_csv("test_groundtruth.csv", sep=' ', names=['id', 'label'])['label'].values
     labels = []
-    # fileLists = os.listdir(Submission_path)
-    # fileLists = [fileLists[i] for i in [0, 5, 6, 8, 9, 11, 12]]
     # 0987
-    fileLists = ['result-0985_vote.csv', 'result-0899-xception.csv', 'result-0920-resnet50.csv', 
-    'result-0941-xception.csv', 'result-0956-mutiscale.csv', 'result-0962-mutiscale_3.csv', 'result-0972-densenet_4c.csv']
+    # fileLists = ['result-0985_vote.csv', 'result-0899-xception.csv', 'result-0920-resnet50.csv', 
+    # 'result-0941-xception.csv', 'result-0956-mutiscale.csv', 'result-0962-mutiscale_3.csv', 'result-0972-densenet_4c.csv']
+    # 0989
+    fileLists = ['result-0987_vote.csv', 'result-0962-mutiscale_3.csv', 'result-0967-densenet.csv', 
+    'result-0972-densenet_4c.csv', 'result-0956-mutiscale.csv', 'result-0941-xception.csv', 'result-0938-CRNN_4c.csv']
 
     read_img_id = False
     for result in fileLists:
@@ -74,9 +76,11 @@ def MergeResult(Submission_path,vote_result):
     vote_label = np.array(vote_label)
 
     #生成csv文件
+    result = true_label == vote_label
+    acc = sum(result.astype(np.int64))
 
     sub = pd.DataFrame({'id': img_id, 'lable': vote_label})
-    sub.to_csv(vote_result, index=False, header=False, sep=' ')
+    sub.to_csv("result-{:0>4}_vote.csv".format(acc), index=False, header=False, sep=' ')
 
 def Compare(vote_result,test_truth):
     new_label = pd.read_csv(vote_result, sep=' ', names=['id', 'label'])['label'].values
@@ -90,8 +94,7 @@ def Compare(vote_result,test_truth):
 if __name__ == '__main__':
     Submission_path = "../Submission"
 
-    vote_result = "vote_result.csv"
     test_truth = "test_groundtruth.csv"
 
-    MergeResult(Submission_path,vote_result)
-    Compare(vote_result,test_truth)
+    MergeResult(Submission_path)
+    # Compare(vote_result,test_truth)
