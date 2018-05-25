7ffcaa0ddd9a64b8f548a221bcb3ee29d0a2d5b1
sfx <shangfangxin0341@link.tyut.edu.cn>
2018-05-26 00:09:09 +0800

    update

---
+++ FeatureEngineering/reindex.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import os
+
+def equal(a, b):
+    return int(a) == int(b)
+
+if __name__ == '__main__':
+    Submission_path = "../Submission"
+    test_truth = "test_groundtruth.csv"
+
+    gt_result = pd.read_csv(test_truth , sep=' ', names=['filepath', 'classid_true'],
+        dtype={"filepath":"str", "classid_true":"int"})
+
+    fileLists = os.listdir(Submission_path)
+    for file in fileLists:
+        if file.split(".")[-1] != "csv":
+            continue
+        result_handle = pd.read_csv(os.path.join(Submission_path,file),sep=' ',names=['filepath','classid_pred'], 
+            dtype={"filepath":"str", "classid_pred":"int"})
+        compare = gt_result.merge(result_handle, how='left', on='filepath')
+        compare.loc[:,["filepath","classid_pred"]].to_csv(file, index=False, header=False, sep=' ')
+
+        # check
+        compare['res'] = compare.apply(lambda row: equal(row['classid_true'], row['classid_pred']), axis=1)
+
+        acc = compare['res'].sum()
+        print("{} - {}".format(file, acc))
--- FeatureEngineering/result_vote.py
+++ FeatureEngineering/result_vote.py
@@ -61,7 +61,10 @@ def MergeResult(Submission_path):
     # fileLists = ['removeable/result-0989_vote.csv', 'removeable/result-0920-resnet50.csv', 'result-0979-densenet_4c_aug.csv', 'result-0991_vote.csv']
     # 0993
     # fileLists = ['result-0962-inception_resv2_4c_aug.csv', 'result-0992_vote.csv', 'result-0970-inception_4c_aug.csv', 'result-0979-densenet_4c_aug.csv', 'result-0991_vote.csv']
-    fileLists = ['result-0961-xception_4c_aug.csv', 'result-0967-densenet.csv', 'result-0992_vote.csv', 'result-0962-nasnet_4c_aug.csv', 'result-0993_vote.csv']
+    # fileLists = ['result-0961-xception_4c_aug.csv', 'result-0949-nasnet_4c_aug.csv', 'result-0992_vote.csv', 'result-0993_vote.csv']
+    # 0994
+    # fileLists = ['result-0961-xception_4c_aug.csv', 'result-0935-CRNN.csv', 'result-0992_vote.csv', 'result-0993_vote.csv', 'result-0979-densenet_4c_aug.csv']
+    fileLists = ['result-0946-inception_resv2_4c.csv', 'result-0961-xception_4c_aug.csv', 'result-0993_vote.csv', 'result-0994_vote.csv']
     read_img_id = False
     for result in fileLists:
         if result.split(".")[-1] != "csv":
