732fdd1e5a8feee6349d30baff2cd9de5487e3ee
slicent <shangfangxin0341@link.tyut.edu.cn>
2018-06-18 22:25:37 +0800

    update

--- second_stage/Classify/Losses.py
+++ second_stage/Classify/Losses.py
@@ -11,7 +11,7 @@ Compatible with tensorflow backend
 def focal_loss(y_true, y_pred):
     alpha=.75
     gamma=2.
-    y_pred=K.clip(y_pred,K.epsilon,1.-K.epsilon)#improve the stability of the focal loss and see issues 1 for more information
+    y_pred=K.clip(y_pred,K.epsilon(),1.-K.epsilon())#improve the stability of the focal loss and see issues 1 for more information
     pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
     pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
     return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
